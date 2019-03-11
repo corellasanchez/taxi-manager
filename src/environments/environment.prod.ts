9e703b4d760883fafb62589fc1a6f40b075377ca
Roy Corella <corellasanchez@hotmail.com>
2019-03-10 18:02:43 -0600

    Firebase login support added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  config : {
+    apiKey: 'AIzaSyB1sIsXHpkmVQZ9zHzV5uhgTNhFF-NzFpA',
+    authDomain: 'taxi-manager-api.firebaseapp.com',
+    databaseURL: 'https://taxi-manager-api.firebaseio.com',
+    projectId: 'taxi-manager-api',
+    storageBucket: 'taxi-manager-api.appspot.com',
+    messagingSenderId: '370633316164'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  config : {
+    apiKey: 'AIzaSyB1sIsXHpkmVQZ9zHzV5uhgTNhFF-NzFpA',
+    authDomain: 'taxi-manager-api.firebaseapp.com',
+    databaseURL: 'https://taxi-manager-api.firebaseio.com',
+    projectId: 'taxi-manager-api',
+    storageBucket: 'taxi-manager-api.appspot.com',
+    messagingSenderId: '370633316164'  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
